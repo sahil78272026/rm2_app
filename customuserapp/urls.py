947b37a6f4c23eafacb5a4376391fcf8ae836d3c
sahil.garg78272026@gmail.com <sahil.garg78272026@gmail.com>
2025-06-05 21:33:47 +0530

    constssssas

--- customuserapp/urls.py
+++ customuserapp/urls.py
@@ -5,7 +5,7 @@ from .views import UserRegistration
 urlpatterns = [
     path('home/', UserRegistration.as_view({'post':'create_user'})),
     path('get_users/', UserRegistration.as_view({'get':'get_users'})),
-    # path('get_users/', UserRegistration.as_view({'get':'get_users'})),
+    path('update_users/', UserRegistration.as_view({'put':'update_user'})),
     # path('get_users/', UserRegistration.as_view({'get':'get_users'})),
     # path('get_users/', UserRegistration.as_view({'get':'get_users'}))
     # path('get_users/', UserRegistration.as_view({'get':'get_users'}))
